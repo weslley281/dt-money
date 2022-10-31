87bdf08b5ac81d61ba19b8ae715d9b9379500abf
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-31 15:07:29 -0400

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-  return <h1>Olá mundo</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Olá mundo</h1>
+    </ThemeProvider>
+  );
 }
