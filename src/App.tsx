cabc96703e3f66988beaeca19b52ebdd832e106d
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-02 08:21:30 -0400

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Transactions } from './pages/transactions';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Olá mundo</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  background-color: ${({ theme }) => theme['gray-900']};
+  padding: 2.5rem 0 7.5rem;
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NewTransactionButton = styled.button`
+  height: 50px;
+  border: 0;
+  background-color: ${({ theme }) => theme['green-500']};
+  font-weight: bold;
+  padding: 0 1.25rem;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme['green-700']};
+  }
+`;
