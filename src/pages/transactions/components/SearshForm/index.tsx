21a788de9c46f4e1f8005ca6489700456b9f37ce
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-12-15 06:49:59 -0400

    a

--- src/pages/transactions/components/SearshForm/index.tsx
+++ src/pages/transactions/components/SearshForm/index.tsx
@@ -1,10 +1,29 @@
 import { MagnifyingGlass } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
 import { SeachFormContainer } from './styles';
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const searchFormSchema = zod.object({
+  query: zod.string(),
+});
+
+type SearchFormInputs = zod.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
+
+  function handleSearchTransactions() {}
+
   return (
-    <SeachFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+    <SeachFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
 
       <button type="submit">
         <MagnifyingGlass size={20} /> Buscar
